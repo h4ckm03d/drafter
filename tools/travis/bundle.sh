3d14323d327c5cb8b1639ce874a2f5973777dc4a
Moch. Lutfi <kapten.lufi@gmail.com>
2019-04-27 11:59:54 +0700

    Release version using latest stable drafter v3.2.7

--- tools/travis/bundle.sh
+++ tools/travis/bundle.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-TRAVIS_ARCHIVE_FILE=drafter-$DRAFTER_VERSION-$TRAVIS_TAG.${TRAVIS_OS_NAME/osx/darwin}-amd64.tar.gz
+TRAVIS_ARCHIVE_FILE=drafter-$TRAVIS_TAG.${TRAVIS_OS_NAME/osx/darwin}-amd64.tar.gz
 
 if [[ "$TRAVIS_TEST_RESULT" == "0" ]]; then
     echo "Generating ${TRAVIS_ARCHIVE_FILE}"
